85d348c830e58d113414da5a3c028a0eca5f5fe6
Chisom <aralu.chisom@gmail.com>
2022-10-13 17:57:07 -0700

    Hello, print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 /* printing using puts */
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ **/
 int main(void) /* main function*/
 {
 	puts('\"Programming is like building a multilingual puzzle"\n');
