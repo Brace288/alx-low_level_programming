303faa4248cf0ffc2575eef39295e44b438c058c
Chisom <aralu.chisom@gmail.com>
2022-10-13 18:25:14 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,10 @@
  * Description: 'the program's description'
  * Return: Always 0 (Success)
  **/
+
 int main(void) /* main function*/
 {
-	puts('\"Programming is like building a multilingual puzzle"\n');
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
 
