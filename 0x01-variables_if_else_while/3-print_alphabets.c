3542afdd0060300f844e258a9131bd4aa94278e9
Chisom <aralu.chisom@gmail.com>
2022-10-17 17:40:06 -0700

    Print both upper and lower cases

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+
+int main(void)
+{
+	char n;
+	char m;
+
+	for (n = 'a'; n <= 'z'; n++)
+		putchar(n);
+
+	for (m = 'A'; m <= 'Z'; m++)
+		putchar(m);
+
+	putchar('\n');
+
+	return(0);
+}
