e7d518663086f4c1443cd8285ac9c07d2f2985ff
Chisom <aralu.chisom@gmail.com>
2022-10-17 17:43:14 -0700

    Print both upper and lower cases

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ * Description: 'Print both upper and lower cases'
+ * Return: Always 0
+ **/
 
 
 int main(void)
@@ -15,5 +19,5 @@ int main(void)
 
 	putchar('\n');
 
-	return(0);
+	return (0);
 }
