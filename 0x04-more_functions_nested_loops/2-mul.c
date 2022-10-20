7af0bae189b16590ab9d7ffdc78fa3991622ff9a
Chisom <aralu.chisom@gmail.com>
2022-10-20 09:08:27 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/*
+ * mul - multiple two integer
+ * @int a: first number
+ * @int b: second number
+ *
+ * R
+ * return: result
+ */
+
+int mul(int a, int b);
+{
+	return (a * b);
+}
