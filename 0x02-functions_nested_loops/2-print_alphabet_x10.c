ad22ed9895d8503c177ee6211186c2b5ac9fa34f
Chisom <aralu.chisom@gmail.com>
2022-10-20 07:54:05 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+
+/**
+ * main - Entry point
+ * Description: prints lower case x10
+ * Return: Always 0
+ **/
+
+
+void print_alphabet_x10(void);
+{
+	int num;
+	char = letter;
+
+	num = 0;
+
+	while (num++ <= 9)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
+	}
+	
