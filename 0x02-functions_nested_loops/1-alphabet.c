39f147aa48f7fa38fb3fba9d5f07d1b8c2b2e1b2
Chisom <aralu.chisom@gmail.com>
2022-10-20 04:23:55 -0700

    task 0

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+void print_alphabet(void)
+{
+	char = lower_case;
+
+	for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
+		_putchar(lower_case);
+
+	_putchar('\n');
+
+	return (0);
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,2 @@
+int _putchar(char m);
+void print_alphabet(void);
