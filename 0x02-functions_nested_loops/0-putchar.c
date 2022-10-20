ba0f274c54caabeafa7bdb1a60b94eb303ee8b1f
Chisom <aralu.chisom@gmail.com>
2022-10-20 07:05:02 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,15 @@
 #include "main.h"
-#include <unistd.h>
-
-
 
 /**
- *
- *  * _putchar - writes the character c to stdout
- *
- *   * @c: The character to print
- *
- *    *
- *
- *     * Return: On success 1.
- *
- *      * On error, -1 is returned, and errno is set appropriately.
- *
- *       */
+ * main - Entry point
+ * Description - writes characters from the function
+ * written: chisom
+ * return: always 1
+ **/
 
 int main(void)
 {
+
 _putchar('_');
 _putchar('p');
 _putchar('u');
@@ -28,6 +19,7 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
+
 return (0);
 
 }
