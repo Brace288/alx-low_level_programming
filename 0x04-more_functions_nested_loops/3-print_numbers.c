081a27510a7b4a4592c17bb80ab49bed3f7e6b88
Chisom <aralu.chisom@gmail.com>
2022-10-20 10:59:33 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/*
+ * print_numbers - prints numbers 0 - 9
+ * return: void
+ */
+
+
+void print_numbers(void);
+{
+	char c;
+
+	for (c = '0'; c < '10'; c++)
+		_putchar(c);
+	_putchar('\n');
