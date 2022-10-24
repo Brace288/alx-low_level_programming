f0d72860316a79a3621886fa04a46447f203a0cf
Chisom <aralu.chisom@gmail.com>
2022-10-24 15:41:53 -0700

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,14 +10,13 @@ int print_last_digit(int n)
 	int x;
 
 	if (n < 0)
-		n = -n;
-
-	x = n % 10;
-
-	if (x < 0)
-		x = -x;
-
-	_putchar (x + '0');
-
-	return (x);
+	{
+		n = (n * (-1));
+		x = n % 10;
+		return (x);
+	}
+	else
+	{	x = n % 10;
+		return (x);
+	}
 }
