458f4e138e9d87a02d3aafffab1ba0885c57e672
Chisom <aralu.chisom@gmail.com>
2022-10-24 14:23:32 -0700

    task 4

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,14 @@
 #include "main.h"
+/**
+ * _islower(int c) - Entry point
+ * Description: checks for lowercase
+ * return: 0 or 1
+ **/
 
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
-			return (1);
+		return (1);
 	else
-			return (0);
-	
+		return (0);
 }
