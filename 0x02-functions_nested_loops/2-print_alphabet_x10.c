d9de66741a651063892bcb170d76a92cbcb465c4
Chisom <aralu.chisom@gmail.com>
2022-10-24 13:41:06 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,7 +10,7 @@
 
 void print_alphabet_x10(void);
 {
-	int num, c;
+	int c, num;
 
 
 	num = 0;
@@ -18,10 +18,10 @@ void print_alphabet_x10(void);
 	while (num <= 9)
 	{
 		for (c = 'a'; c <= 'z'; c++)
+		{
 			_putchar(c);
-
-	}
-	num++;
+		}
+		num++;
 		_putchar('\n');
-	
+	}
 }
